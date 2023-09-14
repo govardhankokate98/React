60a9f1b0cda5c3b7b6e4da8452cfe8824aaa6e41
Govardhan Kokate <govardhankokate98@gmail.com>
2023-09-14 22:39:47 +0530

    inception code push by govardhan on 14 sept 2023

--- inception/App.js
+++ inception/App.js
@@ -1,3 +1,16 @@
-const heading = React.createElement("h1", { id: heading, xyz: abc }, "Hello word by react!")
+const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello word by react!")
+const parent = React.createElement("div", { id: "parent" },
+    [
+        React.createElement("div", { id: "child" },
+            [
+                React.createElement("h1", { id: "head1" }, "Hello word by react!"),
+                React.createElement("h2", { id: "head2" }, "Hello word by react!")
+            ]),
+        React.createElement("div", { id: "child2" },
+            [
+                React.createElement("h1", { id: "head1" }, "Hello word by react!"),
+                React.createElement("h2", { id: "head2" }, "Hello word by react!")
+            ])
+    ])
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+root.render(parent)
